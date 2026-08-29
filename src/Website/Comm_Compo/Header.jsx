3261223277dbfcc7c0c86fa409fb966f5ad05916
Nitya Shah <ncs110905@gmail.com>
2026-08-29 12:03:05 +0530

    Header Improvements

--- src/Website/Comm_Compo/Header.jsx
+++ src/Website/Comm_Compo/Header.jsx
@@ -18,7 +18,6 @@ function Header({ desc }) {
     },[])
     return (
         <div>
-            {/* Topbar Start */}
             <div className="container-fluid bg-dark px-5 d-none d-lg-block">
                 <div className="row gx-0">
                     <div className="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
@@ -39,13 +38,10 @@ function Header({ desc }) {
                     </div>
                 </div>
             </div>
-            {/* Topbar End */}
-            {/* Navbar & Hero Start */}
             <div className="container-fluid position-relative p-0">
                 <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
                     <a href className="navbar-brand p-0">
                         <h1 className="text-primary m-0"><i className="fa fa-map-marker-alt me-3" />Tourist</h1>
-                        {/* <img src="img/logo.png" alt="Logo"> */}
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                         <span className="fa fa-bars" />
@@ -75,7 +71,7 @@ function Header({ desc }) {
                                     if (localStorage.getItem('userid')) {
                                         return (
                                             <>
-                                                <Link to="/proedit" className="nav-item nav-link">{localStorage.getItem("username")}</Link>
+                                                <Link to="/useredit" className="nav-item nav-link">{localStorage.getItem("username")}</Link>
                                             </>
                                         )
                                     }
@@ -119,7 +115,6 @@ function Header({ desc }) {
                     </div>
                 </div>
             </div>
-            {/* Navbar & Hero End */}
         </div>
 
     )
